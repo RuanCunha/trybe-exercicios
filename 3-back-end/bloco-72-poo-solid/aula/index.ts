8c9332154081e70db6c29521bc5d9b65640f379b
RuanCunha <ruanvcunha@hotmail.com>
2022-06-02 18:48:07 -0300

    Sintaxe de Encapsulamento

--- 3-back-end/bloco-72-poo-solid/aula/index.ts
+++ 3-back-end/bloco-72-poo-solid/aula/index.ts
@@ -1,22 +1,37 @@
 class TV {
-  brand: string;
-  size: number;
-  resolution: string;
-  connections: string;
-  connectedTo: string;
+  private _brand: string;
+  private _size: number;
+  private _resolution: string;
+  private _connections: string[];
+  private _connectedTo: string;
 
-  constructor(brand: string, size: number, resolution: string, connections: string, connectedTo: string) {
-    this.brand = brand;
-    this.size = size;
-    this.resolution = resolution;
-    this. connections = connections;
-    this.connectedTo = connectedTo;
+  constructor(brand: string, size: number, resolution: string, connections: string[], connectedTo: string) {
+    this._brand = brand;
+    this._size = size;
+    this._resolution = resolution;
+    this. _connections = connections;
+    this._connectedTo = connectedTo;
+  }
+
+  get connectedTo() {
+    return this._connectedTo;
+  }
+
+  set newConnection(newConnection: string) {
+    if (this._connections.includes(newConnection)) {
+      this._connectedTo = newConnection;
+      console.log(`Connection changed to ${newConnection}`);
+    } else {
+        console.log('Sorry, connection unavailable!');
+    }
   }
 
   turnOn() {
-    console.log(`Televisão ${this.brand} ${this.size}", resolução ${this.resolution}, com conexões ${this.connections}, connectado pela porta ${this.connectedTo}`);
+    console.log(`Televisão ${this._brand} ${this._size}", resolução ${this._resolution}, com conexões ${this._connections.join(', ')}, connectado pela porta ${this._connectedTo}`);
   }
 }
 
-const tv1 = new TV('AOC', 42, '4k', 'HDMI, DisplayPort, VGA', 'HDMI');
-tv1.turnOn;
+const tv1 = new TV('AOC', 42, '4k', ['HDMI', 'DisplayPort', 'VGA'], 'HDMI');
+tv1.turnOn();
+tv1.newConnection = 'VGA'
+tv1.turnOn();
