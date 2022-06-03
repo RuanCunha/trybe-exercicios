4e828030c50a21d154c6595cb42d6c63b4586c4c
RuanCunha <ruanvcunha@hotmail.com>
2022-06-02 21:17:25 -0300

    Exercicios 3 e 4

--- 3-back-end/bloco-72-poo-solid/exercicio/dist/index.js
+++ 3-back-end/bloco-72-poo-solid/exercicio/dist/index.js
@@ -1,4 +1,5 @@
 "use strict";
+// Exercicios 1 e 2
 class estudante {
     constructor(matricula, nome, prova1, prova2, prova3, prova4, trabalho1, trabalho2) {
         this.matricula = matricula;
@@ -22,3 +23,99 @@ console.log("Soma das notas das provas");
 console.log(est1.sum());
 console.log("Média das provas");
 console.log(est1.average());
+// Exercicio 3 e 4
+class Cliente {
+    constructor(nome) {
+        this._nome = nome;
+    }
+    get nome() {
+        return this._nome;
+    }
+    set nome(novoNome) {
+        if (novoNome.length < 3) {
+            throw new Error('Nome muito curto');
+        }
+        this._nome = novoNome;
+    }
+}
+class Item {
+    constructor(nome, preco) {
+        this._nome = nome;
+        this._preco = preco;
+    }
+    get nome() {
+        return this._nome;
+    }
+    set nome(novoNome) {
+        if (novoNome.length < 3) {
+            throw new Error('Nome do item muito curto');
+        }
+        this._nome = novoNome;
+    }
+    get preco() {
+        return this._preco;
+    }
+    set preco(novoPreco) {
+        if (novoPreco <= 0) {
+            throw new Error('Apenas valores positivos');
+        }
+        this._preco = novoPreco;
+    }
+}
+class Pedido {
+    constructor(cliente, itens, metodoPagamento) {
+        this._desconto = 0;
+        this._cliente = cliente;
+        this._itens = itens;
+        this._metodoPagamento = metodoPagamento;
+    }
+    get cliente() {
+        return this._cliente;
+    }
+    set cliente(novoCliente) {
+        this._cliente = novoCliente;
+    }
+    get itens() {
+        return this._itens;
+    }
+    set itens(novosItens) {
+        if (novosItens.length === 0) {
+            throw new Error('O pedido deve constar pelo menos um item');
+        }
+        this._itens = novosItens;
+    }
+    get desconto() {
+        return this._desconto;
+    }
+    set desconto(novoDesconto) {
+        if (novoDesconto < 0) {
+            throw new Error('O desconto deve ser positivo ou zero');
+        }
+        this._desconto = novoDesconto;
+    }
+    get metodoPagamento() {
+        return this._metodoPagamento;
+    }
+    set metodoPagamento(novoMetodo) {
+        this._metodoPagamento = novoMetodo;
+    }
+    total() {
+        return this._itens.reduce((previousValue, item) => {
+            const total = previousValue += item.preco;
+            return total;
+        }, 0);
+    }
+    totalComDesconto() {
+        return this.total() * (1 - this._desconto);
+    }
+}
+const cliente1 = new Cliente('Ruan');
+const item1 = new Item('Misto quente', 5);
+const item2 = new Item('Pizza', 25);
+const pedido1 = new Pedido(cliente1, [item1, item2], "dinheiro");
+console.log(`Pedido do cliente ${pedido1.cliente.nome}`);
+console.log(pedido1.itens);
+console.log('Total:');
+console.log(pedido1.total());
+console.log('Total com desconto:');
+console.log(pedido1.totalComDesconto());
--- 3-back-end/bloco-72-poo-solid/exercicio/src/index.ts
+++ 3-back-end/bloco-72-poo-solid/exercicio/src/index.ts
@@ -35,4 +35,133 @@ console.log(est1.sum());
 console.log("Média das provas");
 console.log(est1.average());
 
+// Exercicio 3 e 4
+
+class Cliente {
+  private _nome: string;
+
+  constructor (nome: string) {
+    this._nome = nome;
+  }
+
+  get nome(): string {
+    return this._nome;
+  }
+
+  set nome(novoNome: string) {
+    if (novoNome.length < 3) {
+      throw new Error('Nome muito curto')
+    }
+    this._nome = novoNome
+  }
+}
+
+class Item {
+  private _nome: string;
+  private _preco: number;
+
+  constructor (nome: string, preco: number) {
+    this._nome = nome;
+    this._preco = preco;
+  }
+
+  get nome(): string {
+    return this._nome
+  }
+
+  set nome(novoNome: string) {
+    if (novoNome.length < 3) {
+      throw new Error('Nome do item muito curto')
+    }
+    this._nome = novoNome
+  }
+
+  get preco(): number {
+    return this._preco;
+  }
+
+  set preco(novoPreco: number) {
+    if (novoPreco <= 0) {
+      throw new Error('Apenas valores positivos')
+    }
+    this._preco = novoPreco
+  }
+}
+
+class Pedido {
+  private _cliente: Cliente;
+  private _itens: Item[];
+  private _desconto = 0;
+  private _metodoPagamento: "crédito" | "débito" | "dinheiro";
+
+  constructor (cliente: Cliente, itens: Item[], metodoPagamento: "crédito" | "débito" | "dinheiro") {
+    this._cliente = cliente;
+    this._itens = itens;
+    this._metodoPagamento = metodoPagamento;
+  }
+
+  get cliente(): Cliente {
+    return this._cliente
+  }
+
+  set cliente(novoCliente) {
+    this._cliente = novoCliente
+  }
+
+  get itens(): Item[] {
+    return this._itens
+  }
+
+  set itens(novosItens: Item[]) {
+    if (novosItens.length === 0) {
+      throw new Error('O pedido deve constar pelo menos um item')
+    }
+    this._itens = novosItens
+  }
+
+  get desconto(): number {
+    return this._desconto
+  }
+
+  set desconto(novoDesconto) {
+    if (novoDesconto < 0) {
+      throw new Error('O desconto deve ser positivo ou zero')
+    }
+    this._desconto = novoDesconto
+  }
+
+  get metodoPagamento(): "crédito" | "débito" | "dinheiro" {
+    return this._metodoPagamento
+  }
+
+  set metodoPagamento(novoMetodo: "crédito" | "débito" | "dinheiro") {
+    this._metodoPagamento = novoMetodo
+  }
+
+  total(): number {
+    return this._itens.reduce((previousValue, item) => {
+      const total = previousValue += item.preco;
+      return total;
+    }, 0)
+  }
+
+  totalComDesconto(): number {
+    return this.total() * (1 - this._desconto)
+  }
+}
+
+const cliente1 = new Cliente('Ruan')
+const item1 = new Item('Misto quente', 5)
+const item2 = new Item('Pizza', 25)
+const pedido1 = new Pedido(cliente1, [item1, item2], "dinheiro")
+
+console.log(`Pedido do cliente ${pedido1.cliente.nome}`);
+console.log(pedido1.itens);
+console.log('Total:');
+console.log(pedido1.total());
+console.log('Total com desconto:');
+console.log(pedido1.totalComDesconto());
+
+
+
 
